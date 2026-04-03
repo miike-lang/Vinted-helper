2ffd31f3b8f13b4b63e5335cb8ad77a846f64272
miike-lang <miike_@live.nl>
2026-04-03 23:37:03 +0200

    Increase JSON body limit to 50mb

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json({ limit: "25mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 // Serveer de frontend
 app.use(express.static(__dirname));
